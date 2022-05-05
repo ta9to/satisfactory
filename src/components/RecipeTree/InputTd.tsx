39d947d7bc5c36b4aa823600eaf17ab9db97f2a6
TAKUTO KUDO <ta9to.kd@gmail.com>
2022-05-05 11:07:30 +0900

    add recipe

--- src/components/RecipeTree/InputTd.tsx
+++ src/components/RecipeTree/InputTd.tsx
@@ -7,7 +7,7 @@ const RecipeTreeInputTd: React.FC<any> = (props) => {
             <td className={"border border-gray-400 px-4 py-2"}>
                 <img width={40} src={props.inputItem.image} alt={props.inputItem.name} />
                 <p>{props.flow}/m</p>
-                {/*<p>{props.inputItem.name_jp}</p>*/}
+                <p>{props.inputItem.name_jp}</p>
                 <RecipeComponent itemName={props.inputItem.name} itemArray={props.itemArray} />
             </td>
         </>
